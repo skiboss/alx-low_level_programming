db365d42fd7dbf5d879eaeae4a3833738263c13c
Oghenemeru Avwemoya <skiboss4life@gmail.com>
2022-03-10 20:51:00 -0800

    C file to calculate size of variable

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,17 +7,19 @@
  */
 int main(void)
 {
-	int intType; // integer type
-	float floatType; // float type
-	long int longType; // long type
-	long long int longLongType; //longLongtype
-	char charType; // character type
+	int intType;
+	float floatType;
+	long int longType; 
+	long long int longLongType; 
+	char charType; 
 
-	// sizeof evaluates the size of the variable
+	/* sizeof evaluates the size of the variable*/
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(charType));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(intType));
-	printf("Size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(longType));
-	printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(longLongType));
+	printf("Size of a long int: %lu bytes(s)\n", 
+			(unsigned long)sizeof(longType));
+	printf("Size of a long long int: %lu bytes(s)\n", 
+			(unsigned long)sizeof(longLongType));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(floatType));
 
 	return(0);
