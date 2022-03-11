53d4461a77cf4b193a60f7f8496aed58b2deba83
Oghenemeru Avwemoya <skiboss4life@gmail.com>
2022-03-10 19:59:12 -0800

    [fix]:print function in betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,10 +5,8 @@
      *
      * Return: Always 0 (Success)
      */
-int main(void) 
+int main(void) /* this function prints the string in the puts() function*/ 
 {
-	/* this function prints the string in the puts() function*/
-
 	puts("Programming is like building a multilingual puzzle\n");
 
 	return (0);
