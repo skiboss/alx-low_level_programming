ae7ac636fa83c15ff3a4d9c6644f88b971b8bc77
Oghenemeru Avwemoya <skiboss4life@gmail.com>
2022-03-11 17:45:31 -0800

    Print upper and lower case of the alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar(alpha);
+	}
+
+	for (alpha = 'A'; alpha <= 'Z'; alpha++)
+	{
+		putchar(alpha);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
