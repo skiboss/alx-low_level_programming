fef43758f53ba89916bf418100854e7e7a15bee8
Oghenemeru Avwemoya <skiboss4life@gmail.com>
2022-03-15 11:39:13 -0700

    Upload 0-putchar and readme file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	putchar('_putchar');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,5 @@
+/* Prototype of _putchar function */
+int _putchar(char c);
+
+/* prototype of print _putchar function */
+void print_putchar(void);
