6e86dbd9f4653b68650b973e3ad04d976c9024ab
Oghenemeru Avwemoya <skiboss4life@gmail.com>
2022-03-15 15:03:02 -0700

    update readme and push task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,7 +10,7 @@
  */
 void print_alphabet_x10(void)
 {
-	int i = 10;
+	int i = 0;
 
 	while (i < 10)
 	{
@@ -21,6 +21,7 @@ void print_alphabet_x10(void)
 			putchar(num);
 		}
 		putchar('\n');
+		i++;
 	}
 }
 
