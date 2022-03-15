f4e1153126ce2dbd5dce31aa3013558725fedcc1
Oghenemeru Avwemoya <skiboss4life@gmail.com>
2022-03-15 14:34:16 -0700

    update header file and task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,16 +2,10 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * main - check the code
+ * print_alphabet - function to print the alphabet in lower case
  *
  * Return: Always 0.
  */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
 void print_alphabet(void)
 {
 	int i;
