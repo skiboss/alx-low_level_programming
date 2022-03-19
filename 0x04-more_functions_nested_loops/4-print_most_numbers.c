65d8903dea6a0436fd722d904ee5c20eb4a67b70
Oghenemeru Avwemoya <skiboss4life@gmail.com>
2022-03-19 09:07:48 -0700

    Upload print most task numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints all integers from 0 to 9 asides 2 and 4
+ * Return: expected output 01356789
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		if ((i != 2) && (i != 4))
+		{
+			_putchar('%d', i)
+		}
+		i++;
+	}
+	_putchar("\n");
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -15,4 +15,6 @@ void print_most_numbers(void);
 
 int _putchar(char c);
 
+void print_most_numbers(void);
+
 #endif /* MAIN_H */
