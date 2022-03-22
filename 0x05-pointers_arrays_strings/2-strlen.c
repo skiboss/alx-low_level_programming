a374277f2cfc370563cb8da8ea03eb87e4d73179
Oghenemeru Avwemoya <skiboss4life@gmail.com>
2022-03-21 22:39:09 -0700

    upload task 3

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strlen - a function to return the length of string
+ * @s: argument
+ * Return: Length of string
+ */
+int _strlen(char *s)
+{
+	int i, length;
+	char word;
+
+	i = 1;
+	length = 0;
+	word = s[];
+
+	while (word != '\0')
+	{
+		length++;
+		word = s[i++];
+	}
+	return (sum);
+}
+	
