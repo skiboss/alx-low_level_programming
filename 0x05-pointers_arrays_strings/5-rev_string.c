72cf3d16f0baa43430db1a9bf91223ac4d18cd36
Oghenemeru Avwemoya <skiboss4life@gmail.com>
2022-03-22 01:03:05 -0700

    upload task 6

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string as the name implies
+ * @s: string to be reversed
+ * Return: reversed string
+ */
+void rev_string(char *s)
+{
+	int i, count;
+
+	char rev = s[0];
+
+	while (s[count] != '\n')
+	{
+		count++;
+	}
+
+	for (i = 0; i < count; i++)
+	{
+		count--;
+		rev = s[i];
+		s[i] = s[count];
+		s[count] = rev;
+	}
+}
