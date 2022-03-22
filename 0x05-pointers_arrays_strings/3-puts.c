767ee3a5128c8f39854aa87a8e1a0acd235bc877
Oghenemeru Avwemoya <skiboss4life@gmail.com>
2022-03-21 22:54:14 -0700

    upload task 4

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - a function to print a string
+ * @str: string to print
+ * Return: string
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] !='\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
