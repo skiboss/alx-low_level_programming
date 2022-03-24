fee92b9265dc1c85b0e25680e80d192ef05aadba
Oghenemeru Avwemoya <skiboss4life@gmail.com>
2022-03-24 16:00:55 -0700

    uploading project more pointers

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,4 +4,7 @@
 /* protocol for string concatenation */
 char *_strcat(char *dest, char *src);
 
+/* protocol for copying a string */
+char *_strncpy(char *dest, char *src, int n);
+
 #endif /* MAIN_H */
