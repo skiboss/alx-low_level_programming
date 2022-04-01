9d07c41db2ddc3677c78d919a0c664c1744d973c
Oghenemeru Avwemoya <skiboss4life@gmail.com>
2022-03-31 17:18:22 -0700

    Add: new files

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - recursive function to print the power of input
+ * @x: base input
+ * @y: power input
+ * Return: base x raised to the power of y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (x ** y);
+}
---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/** squareroot - iterates from 1 to n
+ * @a: n
+ * @b: iteration
+ * Return: 1
+ */
+int squareroot(int a, int b)
+{
+	if (b * b == a)
+		return (b);
+	if (b * b > a)
+	       return (-1);
+	return (squareroot(a, b + 1));
+}
+/**
+ * _sqrt_recursion - return the square root of input
+ * @n: number input
+ * Return: squareroot
+ */
+int _sqrt_recursion(int n)
+{
+	return (squareroot(n, 1));
+}
