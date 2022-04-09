457d475ac60ec0449dab02f5cf2adeaa7d89cedf
Oghenemeru Avwemoya <skiboss4life@gmail.com>
2022-04-09 16:21:35 -0700

    Fixed: header file bug

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,9 +5,13 @@
 
 /* positive_or_negative - checks the status of an input */
 
-void positive_or_negative(i);
+void positive_or_negative(int i);
+
+/* returns the largest number */
 
 int largest_number(int a, int b, int c);
+
+/* prints remaining days in a year */
 
 void print_remaining_days(int month, int day, int year);
 
