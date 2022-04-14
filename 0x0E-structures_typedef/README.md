e3da3c7a6d3f147a8ffc0b9d6dde4e7c099415dd
Oghenemeru Avwemoya <skiboss4life@gmail.com>
2022-04-13 22:43:59 -0700

    Add: initialized dog structure project

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+# Project structures and type definition
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - dog structure
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} Dog;
+#endif /* _DOG_H */
