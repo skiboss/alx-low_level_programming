99a8f09969a88f46342c003d0da8ac7e801b3da2
Oghenemeru Avwemoya <skiboss4life@gmail.com>
2022-05-03 07:43:01 -0700

    Fix: 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 			length++;
 	}
 
-	open_file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	open_file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	write_file = write(open_file, text_content, length);
 
 	if (open_file == -1 || write_file == -1)
