aedb53697cd8af4894165bacbd9d58719173323f
Oghenemeru G. Avwemoya <skiboss4life@gmail.com>
2022-07-25 05:53:30 +0100

    Create 1-dlistint_len.c

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - calculate the number of elements in a linked dlistint_t
+ *
+ * @h: a pointer to head node
+ * Return: the number of elements
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t num_elem = 0;
+
+	while (h)
+	{
+		num_elem++;
+		h = h->next;
+	}
+
+	return (num_elem);
+}
