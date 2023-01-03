cc0940da6bd221f23ce1309551d6118739fea1ed
Oghenemeru Avwemoya <skiboss4life@gmail.com>
2023-01-03 14:47:04 +0100

    fix: 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -22,7 +22,7 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		arr[i] == malloc(width * sizeof(int));
+		arr[i] = malloc(width * sizeof(int));
 		if (arr[i] == NULL)
 		{
 			for (j = 0; j <= i; j++)
