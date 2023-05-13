0591a82f7ef4fdfb3a0984fea8c4f2ec9f45c676
Oghenemeru Avwemoya <skiboss4life@gmail.com>
2023-05-13 22:32:44 +0100

    Modify: library file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -9,5 +9,6 @@ int binary_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array(int *array, int left, int right);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
 
 #endif
